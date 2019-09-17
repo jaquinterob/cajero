18db74220166b9a46aee1a5e32d3d1c66c684db3
John Alexander Quintero Berrío <32532010+jaquinterob@users.noreply.github.com>
2019-09-16 19:48:10 -0500

    Update index.php
    se realiza el conteo linea por linea

--- index.php
+++ index.php
@@ -53,110 +53,110 @@
   <div class="col s12 m8 offset-m2 l4 ">  <!--5-->
     <div class="card">  <!--5-->
       <div class="card-content">  <!--5-->
-        <span class="card-title center-align">Cajero automático</span>
-        <p class="center-align">Bienvenido</p>
-        <div class="row">
-          <div class="input-field col s10 offset-s1">
-            <select class="campo" id="tipo_transaccion">
-              <option value="" disabled selected>Seleccione</option>
-              <option value="consignar">Consignar</option>
-              <option value="retirar">Retirar</option>
-              <option value="consultar_saldo">Consultar saldo</option>
+        <span class="card-title center-align">Cajero automático</span>  <!--6-->
+        <p class="center-align">Bienvenido</p> <!--6-->
+        <div class="row"> <!--5-->
+          <div class="input-field col s10 offset-s1"> <!--5-->
+            <select class="campo" id="tipo_transaccion"> <!--8-->
+              <option value="" disabled selected>Seleccione</option> <!--8-->
+              <option value="consignar">Consignar</option>  <!--6-->
+              <option value="retirar">Retirar</option>  <!--6-->
+              <option value="consultar_saldo">Consultar saldo</option>  <!--6-->
             </select>
-            <label>Seleccione tipo de transacción</label>
+            <label>Seleccione tipo de transacción</label> <!--3-->
           </div>
-          <div id="contenedor_tipo_cuenta" class="input-field col s10 offset-s1 ocultar">
-            <select class="campo" id="tipo_cuenta">
-              <option value="" disabled selected>Seleccione</option>
-              <option value="ahorros">Ahorros</option>
-              <option value="corriente">Corriente</option>
+          <div id="contenedor_tipo_cuenta" class="input-field col s10 offset-s1 ocultar">  <!--7-->
+            <select class="campo" id="tipo_cuenta">  <!--8-->
+              <option value="" disabled selected>Seleccione</option>  <!--5-->
+              <option value="ahorros">Ahorros</option>  <!--6-->
+              <option value="corriente">Corriente</option>  <!--6-->
             </select>
-            <label for="tipo_cuenta">Seleccione tipo de cuenta</label>
+            <label for="tipo_cuenta">Seleccione tipo de cuenta</label>  <!--6-->
           </div>
-          <div class="ocultar col s10 offset-s1" id="consignar">
-            <div class="input-field col s12">
-              <input  id="cuenta_consignar" type="number" class="validate numero_cuenta campo">
-              <label for="cuenta_consignar">Ingrese Cuenta destino</label>
+          <div class="ocultar col s10 offset-s1" id="consignar">  <!--8-->
+            <div class="input-field col s12">  <!--5-->
+              <input  id="cuenta_consignar" type="number" class="validate numero_cuenta campo">  <!--11-->
+              <label for="cuenta_consignar">Ingrese Cuenta destino</label> <!--6-->
             </div>
-            <div class="input-field col s10 offset-s1">
-              <input  id="billetes_10" type="number" class="validate campo">
-              <label for="billetes_10"># de billetes de $10.000</label>
+            <div class="input-field col s10 offset-s1">  <!--5-->
+              <input  id="billetes_10" type="number" class="validate campo">  <!--11-->
+              <label for="billetes_10"># de billetes de $10.000</label> <!--8-->
             </div>
-            <div class="input-field col s10 offset-s1">
-              <input  id="billetes_20" type="number" class="validate campo">
-              <label for="billetes_20"># de billetes de $20.000</label>
+            <div class="input-field col s10 offset-s1">  <!--5-->
+              <input  id="billetes_20" type="number" class="validate campo">  <!--10-->
+              <label for="billetes_20"># de billetes de $20.000</label>  <!--7-->
             </div>
-            <div class="input-field col s10 offset-s1">
-              <input  id="billetes_50" type="number"class="validate campo">
-              <label for="billetes_50"># de billetes de $50.000</label>
+            <div class="input-field col s10 offset-s1"> <!--5-->
+              <input  id="billetes_50" type="number"class="validate campo"> <!--10-->
+              <label for="billetes_50"># de billetes de $50.000</label> <!--6-->
             </div>
-            <div class="input-field col s12 center-align">
-              <a onclick="efectuar_consignacion()" class="waves-effect waves-light btn blue">Consignar</a>
-            </div>
-          </div>
-          <div  class="ocultar col s10 offset-s1" id="retirar">
-            <div class="input-field col s8 offset-s2">
-              <input  id="tarjeta_retirar" type="number" class="validate  retirar   campo">
-              <label for="tarjeta_retirar"> N° tarjeta</label>
-            </div>
-            <div class="input-field col s4 offset-s4">
-              <input  id="pin_retirar" type="password" class="validate retirar  campo">
-              <label for="pin_retirar">PIN</label>
-            </div>
-            <div class="input-field col s8 offset-s2">
-              <input style="font-size:30px;color:red"  id="valor_retirar" type="number" class="validate retirar campo">
-              <label for="valor_retirar">Monto retiro</label>
-            </div>
-            <div class="input-field col s12 center-align">
-              <a onclick="efectuar_retiro('retirar')" class="waves-effect waves-light btn blue">Retirar</a>
+            <div class="input-field col s12 center-align">  <!--5-->
+              <a onclick="efectuar_consignacion()" class="waves-effect waves-light btn blue">Consignar</a>  <!--9-->
             </div>
           </div>
-          <div class="ocultar col s10 offset-s1" id="consultar_saldo">
-            <div class="input-field col s12">
-              <input  id="cuenta_consultar_saldo" type="number" class="validate consultar_saldo  campo">
-              <label for="cuenta_consultar_saldo">N° tarjeta</label>
+          <div  class="ocultar col s10 offset-s1" id="retirar">  <!--8-->
+            <div class="input-field col s8 offset-s2"> <!--5-->
+              <input  id="tarjeta_retirar" type="number" class="validate  retirar   campo">  <!--11-->
+              <label for="tarjeta_retirar"> N° tarjeta</label> <!--6-->
             </div>
-            <div class="input-field col s4 offset-s4">
-              <input  id="pin_consultar_saldo" type="password" class="validate consultar_saldo  campo">
-              <label for="pin_consultar_saldo">PIN</label>
+            <div class="input-field col s4 offset-s4"> <!--4-->
+              <input  id="pin_retirar" type="password" class="validate retirar  campo"> <!--11-->
+              <label for="pin_retirar">PIN</label> <!--7-->
             </div>
-            <div class="input-field col s12 center-align">
-              <a onclick="efectuar_consulta_saldo()" class="waves-effect waves-light btn blue">Consultar</a>
+            <div class="input-field col s8 offset-s2"> <!--4-->
+              <input style="font-size:30px;color:red"  id="valor_retirar" type="number" class="validate retirar campo"> <!--19-->
+              <label for="valor_retirar">Monto retiro</label> <!--6-->
+            </div>
+            <div class="input-field col s12 center-align"> <!--5-->
+              <a onclick="efectuar_retiro('retirar')" class="waves-effect waves-light btn blue">Retirar</a> <!--11-->
+            </div>
+          </div>
+          <div class="ocultar col s10 offset-s1" id="consultar_saldo"> <!--8-->
+            <div class="input-field col s12"> <!--5-->
+              <input  id="cuenta_consultar_saldo" type="number" class="validate consultar_saldo  campo"> <!--11-->
+              <label for="cuenta_consultar_saldo">N° tarjeta</label> <!--6-->
+            </div>
+            <div class="input-field col s4 offset-s4"> <!--5-->
+              <input  id="pin_consultar_saldo" type="password" class="validate consultar_saldo  campo"> <!--11-->
+              <label for="pin_consultar_saldo">PIN</label> <!--7-->
+            </div>
+            <div class="input-field col s12 center-align"> <!--5-->
+              <a onclick="efectuar_consulta_saldo()" class="waves-effect waves-light btn blue">Consultar</a> <!--10-->
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div style="font-family:Helvetica;color:#b2b5b7;font-size:9px;text-align: justify" class="col  l2 offset-l1 hide-on-med-and-down	">
-    Actividad 1. 12 %<br><br>
-    Realizar el algoritmo, diagrama de flujo y programación en Código para el siguiente ejercicio. <br><br>
-    Se tiene un cajero automático que recibe consignaciones, realiza retiros y entrega saldos para dos tipo de
-    cuentas; 1 Cuenta corriente 2 Cuenta de Ahorros. <br><br>
-    Para consignación debe validar cuentas de 11 digito y valor a consignar. Leer dinero y ubicarlo en cajas de
-    dinero. Contar billetes que ingresan de 10,000 cajón 1, billetes de 20.000 cajón 2, billetes de 50.000 cajón
-    3. y debe sumar del valor que tiene el cajero. <br><br>
-    Para el retiro y el saldo debe solicitar tarjeta, clave y pin. Debe consultar si la cuenta tiene saldo y
-    descontarlo. Para retiro debe Descontar billetes que salen de 10,000 cajón 1, billetes de 20.000 cajón 2,
-    billetes de 50.000 cajón 3. y debe restar del valor que tiene el cajero. <br><br>
-    Por ultimo debe entregar recibo de la operación.
+  <div style="font-family:Helvetica;color:#b2b5b7;font-size:9px;text-align: justify" class="col  l2 offset-l1 hide-on-med-and-down	"> <!--18-->
+    Actividad 1. 12 %<br><br> <!--5-->
+    Realizar el algoritmo, diagrama de flujo y programación en Código para el siguiente ejercicio. <br><br> <!--5-->
+    Se tiene un cajero automático que recibe consignaciones, realiza retiros y entrega saldos para dos tipo de <!--1-->
+    cuentas; 1 Cuenta corriente 2 Cuenta de Ahorros. <br><br> <!--5-->
+    Para consignación debe validar cuentas de 11 digito y valor a consignar. Leer dinero y ubicarlo en cajas de <!--1-->
+    dinero. Contar billetes que ingresan de 10,000 cajón 1, billetes de 20.000 cajón 2, billetes de 50.000 cajón <!--1-->
+    3. y debe sumar del valor que tiene el cajero. <br><br> <!--5-->
+    Para el retiro y el saldo debe solicitar tarjeta, clave y pin. Debe consultar si la cuenta tiene saldo y <!--1-->
+    descontarlo. Para retiro debe Descontar billetes que salen de 10,000 cajón 1, billetes de 20.000 cajón 2, <!--1-->
+    billetes de 50.000 cajón 3. y debe restar del valor que tiene el cajero. <br><br> <!--5-->
+    Por ultimo debe entregar recibo de la operación.<!--1-->
   </div>
-  <div  class="hoverable" id="inferior">
-    <div style="margin-top:2%" class="row">
-      <div  class="col  s3 m2 offset-m2 blue center-align">
-        <span id="mostrar_billetes_10"> Billetes de 10.000 = 1</span>
+  <div  class="hoverable" id="inferior"> <!--8-->
+    <div style="margin-top:2%" class="row"> <!--10-->
+      <div  class="col  s3 m2 offset-m2 blue center-align"> <!--5-->
+        <span id="mostrar_billetes_10"> Billetes de 10.000 = 1</span> <!--8-->
       </div>
-      <div class="col s3 m2  green center-align">
-        <span id="mostrar_billetes_20"> Billetes de 20.000 = 1</span>
+      <div class="col s3 m2  green center-align"> <!--5-->
+        <span id="mostrar_billetes_20"> Billetes de 20.000 = 1</span> <!--8-->
       </div>
-      <div class="col s3 m2 purple center-align">
-        <span id="mostrar_billetes_50"> Billetes de 50.000 = 1</span>
+      <div class="col s3 m2 purple center-align"> <!--5-->
+        <span id="mostrar_billetes_50"> Billetes de 50.000 = 1</span> <!--8-->
       </div>
-      <div class="col s3  m2 offset-m1 red center-align">
-        <b><span id="total_disponible"> Saldo = $80000</span></b>
+      <div class="col s3  m2 offset-m1 red center-align"> <!--5-->
+        <b><span id="total_disponible"> Saldo = $80000</span></b> <!--10-->
       </div>
     </div>
   </div>
-  <script type="text/javascript" src="js.js"></script>
+  <script type="text/javascript" src="js.js"></script> <!--8-->
 </body>
 </html>
